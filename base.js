2a74bf6e389c50b0303d4136e921b05047fb0254
Nat <807793720@qq.com>
2018-05-08 20:54:31 +0800

    add cocos base

--- base.js
+++ base.js
@@ -18,6 +18,7 @@ class Base{
         // this.prototype.async(function(){
         //     console.log(123)
         // }).then(()=>{console.log(2234)})
+        console.log(this.prototype.isNative([].concat))
     }
     //产生Undefind数组
     createArray(num=1){
@@ -36,7 +37,9 @@ class Base{
         return array
     }
     warn(str){
-        console.warn(str);
+        console.warn('警告:'+str);
+        // 后期可以加上 代码的出处什么的
+        // 代码出处可以使用webpack 的loader
     }
     //窜输入对象 判断当前类型
     //只判断  一些 基础类型
@@ -90,8 +93,114 @@ class Base{
     promise(){
 
     }
+    //检查origin字符串中有没有 特定的 target字符串  也可使用 lastIndexOf
+    indexOf(origin,target){
+        return  String(origin).indexOf(String(target))===-1?false:true;
+    }
+    //检查2 正则匹配
+    test(target,origin){
+        return new RegExp(target).test(origin);
+    }
+    //检测3 使用search 搜索
+    search(origin,target){
+        // 自己测试的时候只看见 返回数字或者是 -1
+        return  String(origin).search(String(target))===-1?false:true;
+    }
+    // 重复 几次
+    repeat (target, n) {
+        //也可以用 字符串 里面的 repeat 函数
+        return (new Array(n + 1)).join(target);
+    }
+    //判断是否是 系统函数
+    isNative(func){
+        return this.indexOf(func,'native code');
+    }
+    //去掉字符串  前面和 后面的 空白的 字符
+    // 数组扩展
+    trim(str){
+        //return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        //优化
+        return str.replace(/^\s+|\s+$/g, '');
+    }
+    // 主要是 查找  数组里面是否拥有  当前元素
+    arrayContains(target, item){
+        return target.indexOf(item) > -1;//能检测出 数组中是否拥有当前的需要 测试的 值
+    }
+    
+    /**
+     * 下面是es6  里面的字符串新增的 函数
+     * 其实 没有太多的  作用
+     * 需要 的 我们 去记住
+     * */
+    //repeat 由于上面 设置了 原生的 repeat 方法 这里就算了
+    // repeat(str,times){
+    //     return str.repeat(times)
+    // }
+    //for( let XX of XXX)可以使用  for of 循环遍历 字符串
+    // es6 版本 查找 特定的字符串
+    includes(target,origin){
+        //直接 返回  true 或者 false  会区分 大小写
+        return origin.includes(target);
+    }
+    // 查找 特定的 字符串 开始 源 目标 支持从第几个 开始查找
+    beginWith(target,origin,start){
+        return origin.startsWith(target,start) // true
+    }
+    // 查找 特定的 字符串 结尾
+    endwith(target,origin,start){
+        return origin.endsWith(target,start) // true
+    }
+    // 字符串开始补全
+    padStart(origin,target,num){
+        return origin.padStart(target,num);
+    }
+    // 字符串结局补全
+    padEnd(origin,target,num){
+        return origin.padEnd(target,num);
+    }
+    //防止 出现一些常见的 漏洞
+    stringRow(str){
+        //String.raw 方法不是 一个正常的函数
+        return String.raw`${str}`
+        //当然  他也可以是一个正常的 函数
+        // String.raw({ raw: 'test' }, 0, 1, 2);
+        // 't0e1s2t'
+        // 等同于
+        // String.raw({ raw: ['t','e','s','t'] }, 0, 1, 2);
+    }
+    /**
+     * 下面是 一些  创建的 es6 数组的 创建的功能
+     * */
+    //求一个数组的最大值
+    //传入的参数是一个数组
+    mathMax(arr){
+        //原理运用了扩展 运算
+        // es5写法 Math.max.apply(null,arr)
+        return Math.max(...arr);
+    }
+    mathMin(arr){
+        return Math.min(...arr);
+    }
+    //将类数组对象  转化为 真正的数组
+    araryFrom(arrayLike){
+        //得到由值组成的 对应的数据 组成的 数组
+        // es5 的相关写法
+        // [].slice.call(obj)
+        // 也可以 直接使用   Object.values()//同为 es6
+        return Array.from(arrayLike);
+    }
+    //PS: 相同的还有  一些 push、数组复制、数组拼接 等等方法  等以后用到的
+    //find 函数
+    find(origin,target){
+        return origin.find((th) => th===taregt);//返回特定的数字
+    }
 }
 
+
+/**
+ * 下面是别人写的  promise 配合 wx api的原理
+ * 虽然的 简化版的  但还是 有点用处吧
+ * */
 // /**
 //  * 将异步API Promise 化（回调函数 改为 链式调用）。具体优化效果看最下边
 //  * @param {* 微信原生异步API} wxFn
---
+++ cocos_base/base.js
@@ -0,0 +1,287 @@
+// js私有变量 极简实现
+let Symbol  = window.Symbol;
+let idCounter = 0;  //防止 小概率 出现相同的情况 
+if (!Symbol) {
+  Symbol = function Symbol(key) {
+    return `__${key}_${Math.floor(Math.random() * 1e9)}_${++idCounter}__`
+  }
+  Symbol.iterator = Symbol('Symbol.iterator')
+}
+//扩充promise的方法
+Promise.prototype.finally = function (callback) { 
+    var Promise = this.constructor; 
+    return this.then( 
+        function (value) { 
+            Promise.resolve(callback()).then( 
+                 function () { 
+                    return value; 
+                } 
+            ); 
+        }, 
+        function (reason) { 
+             Promise.resolve(callback()).then( 
+                function () { 
+                    throw reason; 
+                } 
+            ); 
+        } 
+    ); 
+}
+window.Symbol = Symbol;
+// 当前实例
+let exp;
+// 这里是 一些函数
+class base {
+    constructor(){
+        // 储存当前对象
+        this.queue=[];           // 不同于微信的 ajax 我个人准备  小游戏的 请求 走的 队列形式 
+        this.queueFlag=false;    // 表示 正在 走当前  的队列
+        this.queueDoubleClick=[];// 为了防止出现 重复点击什么的
+        this.wxlogin=typeof wx !=='undefined' ? (wx&&wx.login):{}; //微信的login
+    }
+    // 测试函数
+    static test(){
+        // let a=this.prototype.promise().then(()=>{
+        //     console.log('成功的 回调');
+        //     //throw new Error('hello');
+        // }).catch(err=>{
+        //     console.log(err)
+        // }).finally(function(){
+        //     console.log('最后执行')
+        // });
+        // // console.log(a)
+    }
+    ///////下面的 这些 都是 直接拿过来 直接用了 可能需要改
+    // 在微信里面 是不会出错的 
+    // 从计算机内存中去我需要的东西
+    getStoragePromisify(obj = {}) {
+        return new Promise((resolve, reject) => {
+          obj.success = (res) => {resolve(res)}
+          obj.fail = (err) => {resolve({data: err})}
+          wx.getStorage(obj)
+        })
+    }
+    Promisify(wxFn, options = {}) {
+        return function middleware(obj = {}) {
+            return new Promise((resolve, reject) => {
+                obj.success = options.success ? options.success(resolve) : (res => { resolve(res) })
+                obj.fail = options.fail ? options.fail(reject) : (err => { reject(err) })
+                wxFn(obj)
+            })
+        }
+    }
+    // 微信登录
+    requestPromisify(obj = {}) {
+        return new Promise((resolve, reject) => {
+            obj.success = res => { resolve(res.data) };
+            obj.fail = err => { reject(err) }
+            if (obj.method && obj.method.toUpperCase() === 'POST') {
+                obj.header = obj.header || {};
+                obj.header['Content-Type'] = 'application/x-www-form-urlencoded';
+            }
+            wx.request(obj)
+        })
+    }
+
+    loginPromisify = this.Promisify(this.wxlogin)  // 小程序登陆
+    /**
+     * 登录方法，先取出本地 Token 检查是否过期，过期的情况下才进行 login
+     * 约定：  登陆后获取到用户信息 均存储在 storage 中： {'userClient': data}
+     *        data 为 object： {Expire: 1, Token: '', UserID: ''}
+     * 返回： 该方法返回 登录接口返回的信息 即含有 Token、UserID 等
+    */
+    login(){  //额 需要把login 也封装到  请求里面吗
+        try{ //微信 小程序的 登陆系统
+            //换取code
+            return this.getStoragePromisify({ key: "userClient" })
+            .then(res => {
+                //console.log(res,'取内存中的token');
+                if (res.data.Token &&res.data.Expire &&new Date().getTime() < res.data.Expire * 1000) {
+                    throw {
+                        code: 1,
+                        msg: "Token 存在且未过期,无需 login",
+                        data: res.data
+                    };
+                }
+            })
+            .then(loginPromisify)
+            .then(res => {
+                //console.log(res.code,'查看code')
+                if (res.code) {
+                return requestPromisify({
+                    url: url.common.login,
+                    method: "POST",
+                    data: {
+                        code: res.code
+                    },
+                    header: {
+                        cookie: `AppKey=${config.appid}`
+                    }
+                });
+                } else {
+                    console.log("用户 login 失败！");
+                    throw res.errMsg;
+                }
+            })
+            .then(res => {
+                //console.log(res,'没有登录，调取登录的结果')
+                if (res.f === 1) {
+                    // res.d: {Token: '', UserID: ''}
+                    wx.setStorage({
+                        key: "userClient",
+                        data: res.d
+                    });
+                    return res.d;
+                } else {
+                    console.log("登录失败");
+                }
+            })
+            .catch(err => {
+                if (err.code) {
+                    return err.data;
+                } else {
+                    console.log(err);
+                }
+            });
+        }catch(err){
+            //不是微信 小游戏的时候的 登陆系统  都不知道是什么 登陆
+        }
+    }
+    // 需要新功能 请求 按照 队列  的形式
+    ajaxQueue(obj={}){
+        return this.login().then(res=>{
+            let Token = res.Token;  // 取得token
+            return  this.ajaxQueueRequest(obj)
+        })
+    }
+    // 第一次 写 若出错 纯属 正常操作 
+    // 当你需要 防止 出现双击  请求发送两遍  这种场景的时候
+    ajaxQueueRequest (obj={}){//ajax 请求promise化
+        //发起  请求
+        //嵌套promise
+        var _this=this;
+        return new Promise((resolve,reject)=>{
+            var xhr = cc.loader.getXMLHttpRequest();  
+            xhr.onreadystatechange = function () {  
+                if (xhr.readyState === 4 && (xhr.status >= 200 && xhr.status < 300)) {  
+                    var respone = xhr.responseText;  
+                    //callback(respone);
+                    resolve(respone);
+                    // 成功的 回调  
+                    //_this.delay()
+                    _this.nextTick.call(_this);   
+                }else if(xhr.readyState === 4 && (xhr.status >= 400 && xhr.status < 600)){// 服务器 返回了错误信息
+                    var respone = xhr.responseText;
+                    reject(respone)
+                    // 服务器 返回失败
+                    _this.nextTick.call(_this);
+                }  
+            };
+            let params;
+            // 为了 一个 特定的 key  防止出现 两次 请求 所以从 GET 请求中分离了开来
+            let str='';
+            for(let key in obj.data){
+                str+=key+'='+obj.data[key]+'&'
+            }
+            str=str.slice(0,-1);//去掉最后一个&
+            if(obj.method==='GET'){  // 当是get 请求的时候 把参数 放在最后
+                obj.url+'?'+str.slice(0,-1);
+            }else if(obj.method==='POST'){// 原生的js 不支持传送对象 序列化
+                params=JSON.stringify(obj.data);
+            }  
+            xhr.open(obj.method||"GET", obj.url, true);  
+            if (cc.sys.isNative) {  
+                xhr.setRequestHeader("Accept-Encoding", "gzip,deflate");  
+            }  
+            // note: In Internet Explorer, the timeout property may be set only after calling the open()  
+            // method and before calling the send() method.  
+            xhr.timeout = 5000;// 5 seconds for timeout  
+            xhr.send.bind(null,params);
+            // 使用 闭包把  当前的 请求信息 加入到队列 里面
+            // 需要检测  当前的请求收在队列里面了 使用了 url为标志位
+            //console.log(_this.queueDoubleClick,213232)
+            !_this.checkQueueDoubleClick(obj.url+str,_this.queueDoubleClick)&&_this.queueDoubleClick.push(obj.url+str)&&_this.queue.push([xhr,params]) 
+            
+            setTimeout(() => {
+                //若队列里面  有长度
+                if(_this.queue.length&&!_this.queueFlag){
+                    //console.log(123)
+                    _this.queueFlag=true; //  队列标志位
+                    let xhr=_this.queue.shift();//[0].send();
+                    //console.log(xhr,23433);
+                    xhr[0].send(xhr[1]);
+                    //console.log();//  调用当前的 队列
+                }
+            }, 0);
+        })
+        
+    }
+    nextTick(){
+        if(this.queue.length){  // 进行下一个队列
+            let nextQueue=_this.queue.shift();
+            nextQueue[0].send(nextQueue[1]);
+        } 
+    }
+    // 纯的请求 不存在队列等等的情况
+    ajax(obj={}){
+        var _this=this;
+        return this.promise((resolve,reject)=>{
+            var xhr = cc.loader.getXMLHttpRequest();  
+            xhr.onreadystatechange = function () {  
+                if (xhr.readyState === 4 && (xhr.status >= 200 && xhr.status < 300)) {  
+                    var respone = xhr.responseText;  
+                    //callback(respone);
+                    resolve(respone);
+                }else if(xhr.readyState === 4 && (xhr.status >= 400 && xhr.status < 600)){// 服务器 返回了错误信息
+                    var respone = xhr.responseText;
+                    reject(respone)
+                }  
+            };  
+            xhr.open(obj.method||"GET", obj.url, true);  
+            if (cc.sys.isNative) {  
+                xhr.setRequestHeader("Accept-Encoding", "gzip,deflate");  
+            }  
+            // note: In Internet Explorer, the timeout property may be set only after calling the open()  
+            // method and before calling the send() method.  
+            xhr.timeout = 5000;// 5 seconds for timeout  
+            
+            xhr.send();
+        })
+    }
+    // 检测当前需要 请求 是否已经在我的队列里面
+    // params 需要传入 队列的使用其情况
+    checkQueueDoubleClick(str,queue){
+        cc.log(str,queue)
+        return queue.some((item)=>{
+            return item === str;
+        })
+    }
+    delay(t, v) {
+        return new Promise(function(resolve) {
+            setTimeout(resolve.bind(null, v), t);
+        });
+    }
+    // 还是 希望 能用道 .then 操作 
+    promise(){
+        return new Promise((resolve,reject)=>{
+            try{
+                resolve();
+            }catch(err){
+                reject();
+            }
+        })
+    }
+    //玩家登陆 系统 验证
+    //web安全 检测 对玩家的输入 进行检测  防止出现XSS攻击 等等的情况
+    //额  这个 情况 待加吧  正常的功能 都写不完  T-T
+    checkPlayInput(str){
+        // 待续
+    }   
+}
+let B; // 实例对象
+// 开启单例模式
+if(!exp){
+    exp=Symbol('exp');
+    B=new base();
+}
+export default B;
